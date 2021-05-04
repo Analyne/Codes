1610a43413c09309b7e0905ef0b5af574b9c2649
Analyne <55157680+Analyne@users.noreply.github.com>
2021-05-03 21:21:22 -0500

    Updated main.tf

--- Azure/Database/CreateDatabase/v2.0.0/main.tf
+++ Azure/Database/CreateDatabase/v2.0.0/main.tf
@@ -12,16 +12,16 @@ data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
-resource "azurerm_sql_server" "server" {
+resource "azurerm_mssql_server" "server" {
   name                         = var.server_name
   resource_group_name          = data.azurerm_resource_group.resource_group.name
   location                     = coalesce(var.location, data.azurerm_resource_group.resource_group.location)
   version                      = var.server_version
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_password
+  minimum_tls_version          = 1.2
   tags                         = var.tags
 }
-
 
 resource "azurerm_sql_database" "db" {
   name                             = var.db_name
@@ -29,7 +29,7 @@ resource "azurerm_sql_database" "db" {
   location                         = coalesce(var.location, data.azurerm_resource_group.resource_group.location)
   edition                          = var.db_edition
   collation                        = var.collation
-  server_name                      = azurerm_sql_server.server.name
+  server_name                      = azurerm_mssql_server.server.name
   create_mode                      = "Default"
   max_size_gb                      = var.max_size_gb
   zone_redundant                   = var.zone_redundant
@@ -42,7 +42,7 @@ resource "azurerm_sql_firewall_rule" "fw" {
   count               = var.ip_count
   name                = var.fw_name[count.index]
   resource_group_name = data.azurerm_resource_group.resource_group.name
-  server_name         = azurerm_sql_server.server.name
+  server_name         = azurerm_mssql_server.server.name
   start_ip_address    = var.start_ip_address[count.index]
   end_ip_address      = var.end_ip_address[count.index]
 }
